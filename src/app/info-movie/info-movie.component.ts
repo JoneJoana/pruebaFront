b06d8995f56740edcb126ad4969329132bcaf174
Joana <joanajanefdez@gmail.com>
2022-09-19 10:37:19 +0200

    info-movie + info-serie

--- src/app/info-movie/info-movie.component.ts
+++ src/app/info-movie/info-movie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TMDBService } from '../_services/prueba-front.service';
 
 @Component({
   selector: 'app-info-movie',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoMovieComponent implements OnInit {
 
-  constructor() { }
+  movie: any;
+  id: any;
+  constructor(private _router: Router,private _route: ActivatedRoute,private tmdbService: TMDBService) { }
 
   ngOnInit(): void {
+    this.id = this._route.snapshot.paramMap.get('id');
+    /* this.tmdbService.getMovieByID(this.id).subscribe(
+      (response) => {
+        this.character = response;
+      },
+      (error) => {
+        console.log('Error al cargar datos');
+      }
+    ); */
+  }
+
+  onBack(): void{
+    this._router.navigate(['/movies']);
   }
 
 }
